b3c1f1a63ffbe4a2d440a7b810a11d08aa0fe62f
Oussama <oelomri.pro@gmail.com>
2024-11-25 18:52:56 +0100

    just a litle touch

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const minus = document.querySelectorAll(".minus");
 const plus = document.querySelectorAll(".plus");
 const text = document.querySelectorAll(".text");
 
-
-
 for (let i = 0; i < plus.length; i++) {
     plus[i].addEventListener("click", function() {
         plus[i].classList.toggle("hide");
