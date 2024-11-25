72bc9d0cbd62fca53539291c0909a2891af09996
Oussama <oelomri.pro@gmail.com>
2024-11-25 18:16:57 +0100

    I applied event in all plus and minus images, i sought help from question already posted in overstackflow 'https://stackoverflow.com/questions/17981437/how-to-add-event-listeners-to-an-array-of-objects' i recognized that i can put event inside a loop, and the loop can't work unless launch the event in spicific place of the array

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const minus = document.querySelector(".minus");
-const plus = document.querySelector(".plus");
-const text = document.querySelector(".text")
+const minus = document.querySelectorAll(".minus");
+const plus = document.querySelectorAll(".plus");
+const text = document.querySelectorAll(".text");
 
-function show() {
-    plus.classList.add("hide")
-    minus.classList.remove("hide")
-    text.classList.remove("hide")
-}
 
-function hide() {
-    plus.classList.remove("hide")
-    minus.classList.add("hide")
-    text.classList.add("hide")
-}
 
-plus.addEventListener("click", show)
-minus.addEventListener("click", hide)
+for (let i = 0; i < plus.length; i++) {
+    plus[i].addEventListener("click", function() {
+        plus[i].classList.toggle("hide");
+        minus[i].classList.toggle("hide");
+        text[i].classList.toggle("hide");
+    });
+
+    minus[i].addEventListener("click", function() {
+        plus[i].classList.toggle("hide");
+        minus[i].classList.toggle("hide");
+        text[i].classList.toggle("hide");
+    });
+  }
