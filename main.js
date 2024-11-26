3e761a9d2d952656d99a70c323cc3a378ce46f7a
Oussama <oelomri.pro@gmail.com>
2024-11-26 19:24:21 +0100

    i did a little, i added keyboard event and i find it a little bit hard, i want to control the flow of showing responses of questions by pressing in arrowDown or arrowUp, i just did the first one. I want make the flow with both in next time, after it, i want to make events in the same time with click, arrowDown and arrowUp. note that i'm just build my logical programming thinking

--- main.js
+++ main.js
@@ -1,17 +1,69 @@
 const minus = document.querySelectorAll(".minus");
 const plus = document.querySelectorAll(".plus");
 const text = document.querySelectorAll(".text");
+let counter = 0;
 
-for (let i = 0; i < plus.length; i++) {
-    plus[i].addEventListener("click", function() {
-        plus[i].classList.toggle("hide");
-        minus[i].classList.toggle("hide");
-        text[i].classList.toggle("hide");
-    });
+// for (let i = 0; i < plus.length; i++) {
+//     plus[i].addEventListener("click", function() {
+//         plus[i].classList.toggle("hide");
+//         minus[i].classList.toggle("hide");
+//         text[i].classList.toggle("hide");
+//     });
 
-    minus[i].addEventListener("click", function() {
-        plus[i].classList.toggle("hide");
-        minus[i].classList.toggle("hide");
-        text[i].classList.toggle("hide");
-    });
-  }
+//     minus[i].addEventListener("click", function() {
+//         plus[i].classList.toggle("hide");
+//         minus[i].classList.toggle("hide");
+//         text[i].classList.toggle("hide");
+//     });
+//   }
+
+
+document.body.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowDown") {
+        if(counter ==4) {
+            counter = 0
+            plus[3].classList.toggle("hide");
+            minus[3].classList.toggle("hide");
+            text[3].classList.toggle("hide");
+        }
+
+        plus[counter].classList.toggle("hide");
+        minus[counter].classList.toggle("hide");
+        text[counter].classList.toggle("hide");
+
+        if(counter >= 1) {
+            plus[counter - 1].classList.toggle("hide");
+            minus[counter - 1].classList.toggle("hide");
+            text[counter - 1].classList.toggle("hide");
+        }
+        counter++;
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     if (counter >= articles.length) {
+//     counter = 0;
+// } else {
+//     plus[counter].classList.toggle("hide");
+//     minus[counter].classList.toggle("hide");
+//     text[counter].classList.toggle("hide");
+    
+// }
+// counter++
