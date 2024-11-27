e3703aff34cdd5007c10261b7660de0a6f36eb23
Oussama <oelomri.pro@gmail.com>
2024-11-27 17:28:00 +0100

    I finished the project i try to make navigation with arrowDown and arrowUp button but i find it difficult for my level now may be latter i will find the solution in allah willing, i will learn and add tailwind CSS and reactJS features in this project in allah willing

--- main.js
+++ main.js
@@ -1,69 +1,22 @@
 const minus = document.querySelectorAll(".minus");
 const plus = document.querySelectorAll(".plus");
 const text = document.querySelectorAll(".text");
-let counter = 0;
+const buttons = document.querySelectorAll("button");
 
-// for (let i = 0; i < plus.length; i++) {
-//     plus[i].addEventListener("click", function() {
-//         plus[i].classList.toggle("hide");
-//         minus[i].classList.toggle("hide");
-//         text[i].classList.toggle("hide");
-//     });
+function hiding(num) {
+    plus[num].classList.toggle("hide");
+    minus[num].classList.toggle("hide");
+    text[num].classList.toggle("hide");
+}
 
-//     minus[i].addEventListener("click", function() {
-//         plus[i].classList.toggle("hide");
-//         minus[i].classList.toggle("hide");
-//         text[i].classList.toggle("hide");
-//     });
-//   }
+for (let i = 0; i < plus.length; i++) {
+    plus[i].addEventListener("click", () => {hiding(i)});
 
+    minus[i].addEventListener("click", () => {hiding(i)});
 
-document.body.addEventListener("keydown", (e) => {
-    if(e.key === "ArrowDown") {
-        if(counter ==4) {
-            counter = 0
-            plus[3].classList.toggle("hide");
-            minus[3].classList.toggle("hide");
-            text[3].classList.toggle("hide");
+    buttons[i].addEventListener("keydown", (e) => {
+        if(e.key === "Enter") {
+            hiding(i);
         }
-
-        plus[counter].classList.toggle("hide");
-        minus[counter].classList.toggle("hide");
-        text[counter].classList.toggle("hide");
-
-        if(counter >= 1) {
-            plus[counter - 1].classList.toggle("hide");
-            minus[counter - 1].classList.toggle("hide");
-            text[counter - 1].classList.toggle("hide");
-        }
-        counter++;
-    }
-    
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     if (counter >= articles.length) {
-//     counter = 0;
-// } else {
-//     plus[counter].classList.toggle("hide");
-//     minus[counter].classList.toggle("hide");
-//     text[counter].classList.toggle("hide");
-    
-// }
-// counter++
+    })
+}
