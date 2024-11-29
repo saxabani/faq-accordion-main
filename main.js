64ca3041e74261eb8cbd0f8b6e9d4eaaba320b99
Oussama <oelomri.pro@gmail.com>
2024-11-29 01:04:22 +0100

    i try to update the solution with the advice and help of my friend amine, to apply the real delegation without loop and add some properties of DOM like nexElemenSibling and others...

--- main.js
+++ main.js
@@ -3,41 +3,38 @@ const plus = document.querySelectorAll(".plus");
 const text = document.querySelectorAll(".text");
 const buttons = document.querySelectorAll("button");
 
-function hiding(num) {
-    plus[num].classList.toggle("hide");
-    minus[num].classList.toggle("hide");
-    text[num].classList.toggle("hide");
+function hiding(val) {
+    plus[val].classList.toggle("hide");
+    minus[val].classList.toggle("hide");
+    text[val].classList.toggle("hide");
 }
+
 // This loop without event delegation
+// for (let i = 0; i < plus.length; i++) {
+//     plus[i].addEventListener("click", () => {hiding(i)});
+
+//     minus[i].addEventListener("click", () => {hiding(i)});
+
+//     buttons[i].addEventListener("keydown", (e) => {
+//         if(e.key === "Enter") {
+//             hiding(i);
+//         }
+//     })
+// }
+
+document.body.addEventListener("click", (e) => {
+    const trg = e.target
+    if(trg.classList.contains("plus") || trg.classList.contains("minus")) {
+        ( trg.parentNode.nextElementSibling|| trg.parentNode.previousElementSibling).classList.toggle("hide")
+        trg.classList.toggle("hide");
+        trg.parentNode.parentNode.nextElementSibling.classList.toggle("hide")
+    }      
+});
+
 for (let i = 0; i < plus.length; i++) {
-    plus[i].addEventListener("click", () => {hiding(i)});
-
-    minus[i].addEventListener("click", () => {hiding(i)});
-
     buttons[i].addEventListener("keydown", (e) => {
         if(e.key === "Enter") {
             hiding(i);
         }
     })
 }
-
-// This loop with event delegation
-
-// for (let i = 0; i < plus.length; i++) {
-//     document.body.addEventListener("click", (e) => {
-//         if(e.target === plus[i]) {
-//             hiding(i);
-//         }    
-//     });
-//     document.body.addEventListener("click", (e) => {
-//         if(e.target === minus[i]) {
-//             hiding(i);
-//         }    
-//     });
-//     document.body.addEventListener("keydown", (e) => {
-//         if(e.key === "Enter" && e.target === buttons[i]) {
-//             hiding(i);
-//         }
-//     })
-// }
-
