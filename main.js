96dd5d0e559c99822c737c861a583cfce8722de4
Oussama <oelomri.pro@gmail.com>
2024-11-29 16:59:54 +0100

    make navigation work with delegavtion event

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-const minus = document.querySelectorAll(".minus");
-const plus = document.querySelectorAll(".plus");
-const text = document.querySelectorAll(".text");
-const buttons = document.querySelectorAll("button");
+// const minus = document.querySelectorAll(".minus");
+// const plus = document.querySelectorAll(".plus");
+// const text = document.querySelectorAll(".text");
+// const buttons = document.querySelectorAll("button");
 
-function hiding(val) {
-    plus[val].classList.toggle("hide");
-    minus[val].classList.toggle("hide");
-    text[val].classList.toggle("hide");
-}
+
+
+// function hiding(val) {
+//     plus[val].classList.toggle("hide");
+//     minus[val].classList.toggle("hide");
+//     text[val].classList.toggle("hide");
+// }
+
+// // Button navigation
+// for (let i = 0; i < plus.length; i++) {
+//     buttons[i].addEventListener("keydown", (e) => {
+//         if(e.key === "Enter") {
+//             hiding(i);
+//         }
+//     })
+// }
 
 // This loop without event delegation
 // for (let i = 0; i < plus.length; i++) {
@@ -22,19 +33,25 @@ function hiding(val) {
 //     })
 // }
 
+
+// Eevent delegation(target) and prevent the bubling
 document.body.addEventListener("click", (e) => {
     const trg = e.target
     if(trg.classList.contains("plus") || trg.classList.contains("minus")) {
         trg.classList.toggle("hide");
-        (trg.nextElementSibling|| trg.previousElementSibling).classList.toggle("hide")
+        (trg.nextElementSibling || trg.previousElementSibling).classList.toggle("hide")
         trg.parentNode.parentNode.nextElementSibling.classList.toggle("hide")
     }      
 });
 
-for (let i = 0; i < plus.length; i++) {
-    buttons[i].addEventListener("keydown", (e) => {
-        if(e.key === "Enter") {
-            hiding(i);
-        }
-    })
-}
+
+// Event delegation with navigation buttons
+document.body.addEventListener("keydown", (e) => {
+    const trg = e.target;
+    if(e.key === "Enter") {
+        const childs = trg.children[0].children[1];
+        childs.children[0].classList.toggle("hide");
+        childs.children[1].classList.toggle("hide");
+        trg.children[1].classList.toggle("hide");
+    }
+})
